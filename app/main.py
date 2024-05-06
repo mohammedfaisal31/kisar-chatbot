67b56e58bae8aa6aa2c73e66c48180db54243499
Mohammed Faisal <mohammedfaisal3366@gmail.com>
2024-05-06 22:42:14 +0530

    As

--- app/main.py
+++ app/main.py
@@ -1,42 +1,50 @@
-from fastapi import FastAPI, Request, HTTPException,Form
+from fastapi import APIRouter, FastAPI, Request, HTTPException, Form
 from typing import Dict
 import requests
 from dotenv import load_dotenv
 import os
 from db import *
-from utils import processWhatsAppMessage,processPayment
+from utils import processWhatsAppMessage, processPayment
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-load_dotenv()
 
+load_dotenv()
 
 verify_token = os.getenv("VERIFY_TOKEN")
 
+# Create a router with prefix "/kisar"
+kisar_router = APIRouter(prefix="/kisar")
+
+# Define your FastAPI app
 app = FastAPI()
 
-app.mount("/template", StaticFiles(directory="./template"), name="template")
-app.mount("/pdfs", StaticFiles(directory="./pdfs"), name="pdfs")
+# Mount static directories
+app.mount("/kisar/template", StaticFiles(directory="./template"), name="template")
+app.mount("/kisar/pdfs", StaticFiles(directory="./pdfs"), name="pdfs")
 
-
-@app.post("/webhook")
+# Endpoint for /kisar/webhook
+@kisar_router.post("/webhook")
 async def webhook(request: Request):
     body = await request.json()
     print(body)
     response = processWhatsAppMessage(body)
     return {"status": "ok"}
 
-@app.post("/payment-webhook")
+# Endpoint for /kisar/payment-webhook
+@kisar_router.post("/payment-webhook")
 async def paymentWebhook(payload: Request):
     data = await payload.form()
-    #print(data["amount"])
+    # print(data["amount"])
     result = processPayment(data)
     return {"message": "Webhook received successfully"}
 
-@app.get("/payment-success")
-async def paymentSucess():
+# Endpoint for /kisar/payment-success
+@kisar_router.get("/payment-success")
+async def paymentSuccess():
     return FileResponse("paymentSuccess.html")
 
-@app.get("/webhook")
+# Endpoint for verification of webhook
+@kisar_router.get("/webhook")
 async def verify_webhook(request: Request):
     hub = request.query_params
     if hub["hub.mode"] == "subscribe" and hub["hub.verify_token"] == verify_token:
@@ -44,6 +52,9 @@ async def verify_webhook(request: Request):
     else:
         raise HTTPException(status_code=403, detail="Invalid token")
 
+# Include the kisar_router in the main app
+app.include_router(kisar_router)
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="77.37.44.233", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
